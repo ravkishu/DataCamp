f17e6d0f26ef294b49d631bef54c6059d541dd76
ravkishu <39808656+ravkishu@users.noreply.github.com>
2020-04-30 19:13:18 +0530

    How does the shell compare to a desktop interface?.sh
    How does the shell compare to a desktop interface?

---
+++ Introduction to Shell/01  Manipulating files and directories/How does the shell compare to a desktop interface?.sh
@@ -0,0 +1,30 @@
+An operating system like Windows, Linux, or Mac OS is a special kind of program. It controls the computer's processor, hard drive, and network connection, but its most important job is to run other programs.
+
+Since human beings aren't digital, they need an interface to interact with the operating system. The most common one these days is a graphical file explorer, which translates clicks and double-clicks into commands to open files and run programs. Before computers had graphical displays, though, people typed instructions into a program called a command-line shell. Each time a command is entered, the shell runs some other programs, prints their output in human-readable form, and then displays a prompt to signal that it's ready to accept the next command. (Its name comes from the notion that it's the "outer shell" of the computer.)
+
+Typing commands instead of clicking and dragging may seem clumsy at first, but as you will see, once you start spelling out what you want the computer to do, you can combine old commands to create new ones and automate repetitive operations with just a few keystrokes.
+
+What is the relationship between the graphical file explorer that most people use and the command-line shell?
+
+Answer the question
+-------------------
+Possible Answers
+The file explorer lets you view and edit files, while the shell lets you run programs.
+press 1
+
+The file explorer is built on top of the shell.
+press 2
+
+The shell is part of the operating system, while the file explorer is separate.
+press 3
+
+They are both interfaces for issuing commands to the operating system.
+press 4
+
+Answer
+======
+They are both interfaces for issuing commands to the operating system.
+press 4
+
++50 XP
+Correct! Both take the user's commands (whether typed or clicked) and send them to the operating system.
