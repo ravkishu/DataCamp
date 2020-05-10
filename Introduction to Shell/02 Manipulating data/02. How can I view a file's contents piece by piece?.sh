b3c7b815a59299eab0baefd233924b5261bdf861
ravkishu <39808656+ravkishu@users.noreply.github.com>
2020-05-10 14:18:57 +0530

    How can I view a file's contents piece by piece?

--- Introduction to Shell/02 Manipulating data/02. How can I view a file's contents piece by piece?.sh
+++ Introduction to Shell/02 Manipulating data/02. How can I view a file's contents piece by piece?.sh
@@ -1,6 +1,6 @@
 How can I view a file's contents piece by piece?
 
-You can use /cat/i to print large files and then scroll through the output, but it is usually more convenient to page the output. The original command for doing this was called more, but it has been superseded by a more powerful command called less. (This kind of naming is what passes for humor in the Unix world.) When you less a file, one page is displayed at a time; you can press spacebar to page down or type q to quit.
+You can use `cat` to print large files and then scroll through the output, but it is usually more convenient to page the output. The original command for doing this was called more, but it has been superseded by a more powerful command called less. (This kind of naming is what passes for humor in the Unix world.) When you less a file, one page is displayed at a time; you can press spacebar to page down or type q to quit.
 
 If you give less the names of several files, you can type :n (colon and a lower-case 'n') to move to the next file, :p to go back to the previous one, or :q to quit.
 
