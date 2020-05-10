6cebdf3e5c295ed767592d93163aaa8bea46a030
ravkishu <39808656+ravkishu@users.noreply.github.com>
2020-05-10 11:58:20 +0530

    How can I move to another directory?

---
+++ Introduction to Shell/01  Manipulating files and directories/05. How can I move to another directory?.sh
@@ -0,0 +1,29 @@
+How can I move to another directory?
+Just as you can move around in a file browser by double-clicking on folders, you can move around in the filesystem using the command cd (which stands for "change directory").
+
+If you type cd seasonal and then type pwd, the shell will tell you that you are now in /home/repl/seasonal. If you then run ls on its own, it shows you the contents of /home/repl/seasonal, because that's where you are. If you want to get back to your home directory /home/repl, you can use the command cd /home/repl.
+
+Instructions 1/3
+35 XP
+1
+You are in /home/repl/. Change directory to /home/repl/seasonal using a relative path.
+
+$ cd seasonal/
+
+
+Instructions 2/3
+35 XP
+2. Use pwd to check that you're there.
+
+$ pwd
+
+
+Instructions 3/3
+30 XP
+3. Use ls without any paths to see what's in that directory.
+
+$ ls
+
+
+ +100 XP
+Neat! This was about navigating down to subdirectories. What about moving up? Let's find out!
